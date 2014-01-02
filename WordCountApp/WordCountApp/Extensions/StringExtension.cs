9f32eddf246195e6e9a0388a58eb8768db415b02
morrisondots <morrisondots@gmail.com>
2014-01-02 21:51:07 +0000

    Initial Version

---
+++ WordCountApp/WordCountApp/Extensions/StringExtension.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+ 
+namespace WordCountApp.Extensions
+{
+    //Implemented as an extesion method to allow for usage across strings
+    public static class StringExtension
+    {
+        //local variable to hold the valid characters regular expression
+        private static readonly string ValidCharactersRegEx;
+
+        /// <summary>
+        /// Initializes the <see cref="StringExtension"/> class.
+        /// </summary>
+        static StringExtension()
+        {
+            //Get the valid character regular expression from config to allow for amendments without a recompile.
+            ValidCharactersRegEx = ConfigurationManager.AppSettings["RegExValidChars"];
+        }
+ 
+        /// <summary>
+        /// Strips the non alpha numeric characters.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static string StripCharacters(this string value)
+        {
+            //Remove any characters that do not match the supplied regular expression
+            //If the ValidCharactersRegEx is null, use an Empty String
+            return new Regex(ValidCharactersRegEx ?? String.Empty).Replace(value, String.Empty);
+        }
+ 
+        /// <summary>
+        /// Gets the word count.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns></returns>
+        public static string GetWordCount(this string value)
+        {
+            var result = new StringBuilder();
+
+            //Get a valid words
+            var words = StripCharacters(value);
+
+            //only process if we have valid words
+            if (!(String.IsNullOrWhiteSpace(words)))
+            {
+                //group the words by their lower case values and get their number of occurences
+                var results = words.Split(' ')
+                                   .GroupBy(x => x.ToLowerInvariant())
+                                   .OrderByDescending(x => x.Count());
+
+                //get the results containing words and their number of usages
+                foreach (var item in results)
+                {
+                    result.AppendLine(string.Format("{0} - {1}", item.Key, item.Count()));
+                }
+            }
+            else
+            {
+                result.AppendLine("No valid words supplied.");
+            }
+
+            return result.ToString();
+        }  
+    }
+}
---
+++ WordCountApp/WordCountApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using WordCountApp.Extensions;
+
+namespace WordCountApp
+{
+    class Program
+    {
+        //Default sentence value supplied in requirements spec
+        private const string DefaultValue = "This is a statement, and so is this.";
+
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            string sentence;
+            
+            //If not argument supplied then use the default sentence value
+            if (args.Length == 0)
+            {
+                sentence = DefaultValue;
+            }
+            else if(!(String.IsNullOrWhiteSpace((args[0]))))
+            {
+                //use another sentence if supplied as an argument
+                sentence = args[0];
+            }
+            else
+            {
+                //Output a message to the user informing them that they have supplied an incorrect argument 
+                Console.WriteLine("Please supply only one quoted parameter value to use as a custom sentence value.");
+
+                //Wait for user feedback
+                Console.WriteLine("Press enter key to continue...");
+                Console.ReadLine();
+                return;
+            }
+
+            var result = sentence.GetWordCount();
+            //Show supplied input value
+            Console.WriteLine(String.Format("Input: \"{0}\"", sentence));
+            
+            //Show ouput value
+            Console.WriteLine(String.Format("Output:"));
+            Console.WriteLine(result);
+
+            //Wait for user feedback
+            Console.WriteLine("Press enter key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
